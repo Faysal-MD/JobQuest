8a24d521f26ff3b9b770984064b927faa7588de7
Faysal598 <faysalmahmud735@gmail.com>
2025-01-04 23:30:13 +0600

    Database schema added

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express, { urlencoded } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config({});
 
 const app = express();
 
@@ -21,8 +24,9 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running at port ${PORT}`);
 });
